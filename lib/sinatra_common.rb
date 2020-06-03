25b1620adbce3db4e37467c6fddd03861f6c9f37
Victor Maslov <nakilon@gmail.com>
2020-06-03 18:09:26 +0300

    disabled Error Reporting

--- lib/sinatra_common.rb
+++ lib/sinatra_common.rb
@@ -5,39 +5,34 @@ set :inline_templates, caller.first[/[^:]+/]    # https://stackoverflow.com/q/42
 set :app_file, caller.first[/[^:]+/]            # https://stackoverflow.com/a/10694969/322020
 
 set :bind, "0.0.0.0"
-set :port, (ENV["PORT"] || fail("no ENV['PORT'] specified"))
+set :port, ENV.fetch("PORT", 8001)
 enable :lock
 
-set :environment, :production unless Gem::Platform.local.os == "darwin"
+set :environment, :production unless "TRUE" == ENV["TEST"]
 if development?
   require "pp"
   # require "sinatra/reloader"
   # register Sinatra::Reloader
+else
+  disable :show_exceptions
 end
-disable :show_exceptions
 
 set :public_folder, "public"
 
 # set :views, __dir__
 
 
-require "google/cloud/error_reporting"
-Google::Cloud::ErrorReporting.configure do |config|
-  config.keyfile = ENV["ERROR_REPORTING_KEYFILE"] || fail("no ENV['ERROR_REPORTING_KEYFILE'] specified")
-  config.project_id = JSON.load(File.read ENV["ERROR_REPORTING_KEYFILE"])["project_id"]
-end
 
 # use this class instead of `halt 500, "text"` if you want it to be rendered
-class PublicError < RuntimeError ; end
 
 not_found do                                                                    ; end
 # error   do JSON.dump({status: :error, message: env["sinatra.error"].message}) ; end
+class PublicError < RuntimeError ; end
 error     do
   begin
-    Google::Cloud::ErrorReporting.report env["sinatra.error"]
     next env["sinatra.error"].to_s if PublicError === env["sinatra.error"]
   rescue => e
     puts e, e.backtrace
   end
   "smth went wrong"
-end
+end unless development?
--- sinatra_common.gemspec
+++ sinatra_common.gemspec
@@ -3,8 +3,7 @@ Gem::Specification.new do |spec|
   spec.summary = "sinatra_common"
   spec.author = "Victor Maslov aka Nakilon"
   spec.email = "nakilon@gmail.com"
-  spec.version = "0.0.0.2"
+  spec.version = "0.0.0.3"
   spec.require_path = "lib"
   spec.add_dependency "sinatra", "~>1.4.8"
-  spec.add_dependency "google-cloud-error_reporting"
 end
